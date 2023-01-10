da483225c1ea57a3f22f66781c13cc8455d645df
Mirza2022 <mirzashah2014@gmail.com>
2023-01-10 18:10:03 -0400

    tests

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 require_once('vendor/autoload.php');
 $stripe = array(
-    "secret_key" => 'sk_test_51MFroPLk0H4R8Ms6mnWqNHdD6SaYNjn4kfdACseBW0BpJ1VGsstyppHGSq0yAV0MItCDXwLNVK8zmcRDwhHF8otq00CeSzVYsM',
-    "publishable_key" => 'pk_test_51MFroPLk0H4R8Ms6n48TegNHvso3m4dKaO1n3SU8dYBfWqzWmFpo4ekOeR2HEeaK0Sv9tcUvUqxclwaMN0m9N7bq00cUFRXXlR'
+    "secret_key" => 'sk_test_51MFroPLk0H4R8Ms64f6irJxtfKzZoy1GjYcYcEbK0IRYqsEwt2BDKOINIx4yZuWh1wrqJCrbFvcDuKmM8XxeNCJL00vAgWowyo',
+    "publishable_key" => 'pk_test_51MFroPLk0H4R8Ms6PoA9ECN4TjiLPexGTJZNfcxEfJu4IERFqp6gpznQWSarMbfCd3quaO6t0oNy34zvgd8ee3K200zYXoElW2'
 );
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
--- view/header.php
+++ view/header.php
@@ -41,10 +41,10 @@
                             <?php } ?>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="view/aboutPage.php">About</a>
+                                <a class="nav-link" href="./about.php">About</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="view/servicesPage.php">Services</a>
+                                <a class="nav-link" href="./services.php">Services</a>
                             </li>
 
                             <li class="nav-item">
